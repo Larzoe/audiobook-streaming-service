1f645c82c702aace6d352ab03571613db2f7c32b
Hessel Tjeerdsma <hessel@tjeerdsma.eu>
2024-05-23 14:05:20 +0200

    feat: load notifcation url from env

--- payments/notification_client.py
+++ payments/notification_client.py
@@ -1,7 +1,11 @@
 import requests
+import os
+
+CL_RUN_URL = os.environ["CL_URL"]
+
 
 def send_notification(message):
-    url = 'https://your-notification-service-url/send-notification'
-    payload = {'message': message}
+    url = f"{CL_RUN_URL}/send-notification"
+    payload = {"message": message}
     response = requests.post(url, json=payload)
-    return response.json().get('status')
+    return response.json().get("status")
